e263d604da05778181fde009001d19691ffd4da3
keithoma <thomakmj@gmail.com>
2019-05-24 16:45:58 +0200

    clean uped cmake, formated main and started working on it
    somehow i cant ignore the .vscode files ... will work on on the next push

--- sgfx/examples/ppm/main.cpp
+++ sgfx/examples/ppm/main.cpp
@@ -9,16 +9,17 @@ int main(int argc, char* argv[])
 {
 	using namespace sgfx;
 	
-	window main_window{640,480};
+	window main_window{640, 480};
 	
-	auto bg_img=load_ppm("sample_bg.ppm");
-	point bg_pos{0,0};
-	int scroll_speed=2;
+	auto bg_img = load_ppm("sample_bg.ppm");
+	point bg_pos{0, 0};
+	int scroll_speed = 2;
 
-	auto fg_img=load_ppm("sample_fg.ppm");
-	point fg_pos{main_window.width()/2-fg_img.width()/2,main_window.height()/2-fg_img.height()/2};
+	auto fg_img = load_ppm("sample_fg.ppm");
+	point fg_pos{main_window.width() / 2 - fg_img.width() / 2,
+	             main_window.height() / 2 - fg_img.height() / 2};
 	
-	bool plus_released=true;
+	bool plus_released = true;
 	while(main_window.handle_events() && !main_window.should_close())
 	{
 		if(main_window.is_pressed(key_id{'w'}))
@@ -45,12 +46,13 @@ int main(int argc, char* argv[])
 		else
 			plus_released=true;
 		
-		bg_pos.y=(bg_pos.y+scroll_speed)%bg_img.height();
+		bg_pos.y = (bg_pos.y+scroll_speed) % bg_img.height();
 		
-		draw(main_window,bg_img,bg_pos);
-		draw(main_window,bg_img,bg_pos-vec{0,bg_img.height()});
+		draw(main_window, bg_img, bg_pos);
+		draw(main_window, bg_img, bg_pos - point{0, bg_img.height()});
 		
-		draw(main_window,fg_img,fg_pos,color::cyan);
+		// deleted last parameter "color::cyan"
+		draw(main_window, fg_img, fg_pos);
 		
 		main_window.show();
 	};
