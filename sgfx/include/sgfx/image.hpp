7a08eb739aea5cf7d2e46450effcaf9fa9d69005
Christian Parpart <christian@parpart.family>
2019-05-25 21:53:37 +0200

    coding style

--- sgfx/include/sgfx/image.hpp
+++ sgfx/include/sgfx/image.hpp
@@ -7,19 +7,22 @@
 
 #include <string>
 
-namespace sgfx
-{
-	//would be better to use std::filesystem::path, but support seems to be lacking on some platforms(...) and it seems like not everbody is willing to use the VM xD
-	canvas load_ppm(const std::string& path);
-	void save_ppm(widget const& source, const std::string& path);
-	
-	class rle_image{};
-	
-	rle_image load_rle(const std::string& path);
-	void save_rle(const rle_image& source, const std::string& path);
-	rle_image rle_encode(widget& source);
-	void draw(widget& target, const rle_image& source, point top_left);
-	void draw(widget& target, const rle_image& source, point top_left, color::rgb_color colorkey);
-}
+namespace sgfx {
+
+// would be better to use std::filesystem::path, but support seems to be lacking on some platforms(...) and it
+// seems like not everbody is willing to use the VM xD
+canvas load_ppm(const std::string& path);
+void save_ppm(widget const& source, const std::string& path);
+
+class rle_image {
+};
+
+rle_image load_rle(const std::string& path);
+void save_rle(const rle_image& source, const std::string& path);
+rle_image rle_encode(widget& source);
+void draw(widget& target, const rle_image& source, point top_left);
+void draw(widget& target, const rle_image& source, point top_left, color::rgb_color colorkey);
+
+}  // namespace sgfx
 
 #endif
