8bb0bceaeba583b9a47557e7f86b64c74348144e
Christian Parpart <christian@parpart.family>
2019-05-25 21:51:26 +0200

    coding style conformance

--- examples/rle/main.cpp
+++ examples/rle/main.cpp
@@ -7,22 +7,22 @@
 
 int main(int argc, char* argv[])
 {
-	using namespace sgfx;
-	using namespace std::chrono_literals;
+    using namespace sgfx;
+    using namespace std::chrono_literals;
 
-	window main_window{1024,768};
-	
-	auto bg=load_rle("sample_bg.ppm.rle");
-	auto fg=load_rle("sample_fg.ppm.rle");
+    window main_window{1024, 768};
 
-	draw(main_window,bg,{0,0});
-	draw(main_window,fg,{200,200},color::gray);
+    auto bg = load_rle("sample_bg.ppm.rle");
+    auto fg = load_rle("sample_fg.ppm.rle");
 
-	main_window.show();
-	std::this_thread::sleep_for(5s);
-	
-	auto encoded=rle_encode(main_window);
-	save_rle(encoded, "screenshot.rle");
+    draw(main_window, bg, {0, 0});
+    draw(main_window, fg, {200, 200}, color::gray);
 
-	return 0;
+    main_window.show();
+    std::this_thread::sleep_for(5s);
+
+    auto encoded = rle_encode(main_window);
+    save_rle(encoded, "screenshot.rle");
+
+    return 0;
 }
