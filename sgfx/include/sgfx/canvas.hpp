fad782e0b53d70dfaeac44cc1ac2f7afa5c5c705
Christian Parpart <christian@parpart.family>
2019-05-25 02:43:18 +0200

    canvas: adds second ctor (for ease use) and perf

--- sgfx/include/sgfx/canvas.hpp
+++ sgfx/include/sgfx/canvas.hpp
@@ -4,24 +4,33 @@
 #include <sgfx/primitive_types.hpp>
 #include <sgfx/widget.hpp>
 #include <sgfx/window.hpp>
+#include <stdexcept>
 
 namespace sgfx {
 
 class canvas : public widget {
   public:
-		explicit canvas(dimension size) : size_{size}, pixels_{static_cast<unsigned>(size.width * size.height)} {}
+    using Color = color::rgb_color;
+    using Data = std::vector<Color>;
 
-		std::uint16_t width() const noexcept override { return size_.width; }
-		std::uint16_t height() const noexcept override { return size_.height; }
+    canvas(dimension dim, Data data) : size_{dim}, pixels_{std::move(data)} {
+        if (static_cast<std::size_t>(dim.width * dim.height) != pixels_.size())
+            throw std::invalid_argument("Pixel dimensions don't match image data.");
+	}
 
-		std::vector<color::rgb_color>& pixels() noexcept override { return pixels_; }
-		const std::vector<color::rgb_color>& pixels() const noexcept override { return pixels_; }
+    explicit canvas(dimension size) : size_{size}, pixels_{static_cast<unsigned>(size.width * size.height)} {}
 
-		static canvas colored(dimension size, color::rgb_color col);
+    std::uint16_t width() const noexcept override { return size_.width; }
+    std::uint16_t height() const noexcept override { return size_.height; }
+
+    std::vector<color::rgb_color>& pixels() noexcept override { return pixels_; }
+    const std::vector<color::rgb_color>& pixels() const noexcept override { return pixels_; }
+
+    static canvas colored(dimension size, color::rgb_color col);
 
   private:
-		const dimension size_;
-		std::vector<color::rgb_color> pixels_;
+    const dimension size_;
+    std::vector<color::rgb_color> pixels_;
 };
 
 void draw(widget& target, const canvas& img, point top_left);
