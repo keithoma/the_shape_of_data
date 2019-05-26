cbf984fa294d179ca949838a3371f93f8db088e6
Christian Parpart <christian@parpart.family>
2019-05-26 03:33:05 +0200

    color: implements == != operators; make use of array<> too (makes me happier)

--- sgfx/include/sgfx/color.hpp
+++ sgfx/include/sgfx/color.hpp
@@ -1,25 +1,33 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 
 namespace sgfx::color {
 
 class rgb_color {
   public:
-	constexpr rgb_color() : rgb_color(0, 0, 0) {}
+    constexpr rgb_color() : rgb_color(0, 0, 0) {}
 
-	constexpr rgb_color(std::uint8_t r, std::uint8_t g, std::uint8_t b) : values_{r, g, b} {};
+    constexpr rgb_color(std::uint8_t r, std::uint8_t g, std::uint8_t b) : values_{r, g, b} {};
 
-	constexpr auto& red() { return values_[0]; }
-	constexpr auto& green() { return values_[1]; }
-	constexpr auto& blue() { return values_[2]; }
+    constexpr auto& red() { return values_[0]; }
+    constexpr auto& green() { return values_[1]; }
+    constexpr auto& blue() { return values_[2]; }
 
-	constexpr const auto& red() const { return values_[0]; }
-	constexpr const auto& green() const { return values_[1]; }
-	constexpr const auto& blue() const { return values_[2]; }
+    constexpr const auto& red() const { return values_[0]; }
+    constexpr const auto& green() const { return values_[1]; }
+    constexpr const auto& blue() const { return values_[2]; }
+
+    constexpr bool operator==(const rgb_color& other) const noexcept
+    {
+        return red() == other.red() && green() == other.green() && blue() == other.blue();
+    }
+
+    constexpr bool operator!=(const rgb_color& other) const noexcept { return !(*this == other); }
 
   private:
-	std::uint8_t values_[3];
+    std::array<std::uint8_t, 3> values_;
 };
 
 inline constexpr const rgb_color white{255, 255, 255};
