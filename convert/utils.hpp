d54c774017e612c5040221d44860662b50fdbab3
Christian Parpart <christian@parpart.family>
2019-05-29 02:47:09 +0200

    utility (ranges) helpers

--- convert/utils.hpp
+++ convert/utils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <iterator>
 
 namespace ranges {
 
@@ -8,6 +9,24 @@ template <typename Container, typename F>
 auto min_element(Container& container, F f)
 {
     return std::min_element(begin(container), end(container), f);
+}
+
+template <typename Container, typename OutputIterator>
+void copy(Container& input, OutputIterator output)
+{
+    std::copy(begin(input), end(input), output);
+}
+
+template <typename Container, typename F>
+void for_each(Container& container, F f)
+{
+    std::for_each(begin(container), end(container), f)
+}
+
+template <typename Container, typename F>
+void for_each(Container const& container, F f)
+{
+    std::for_each(cbegin(container), cend(container), f);
 }
 
 }  // namespace ranges
