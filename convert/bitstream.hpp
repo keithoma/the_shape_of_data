ccee28886a716435ac803bcc758a28590e72c101
Christian Parpart <christian@parpart.family>
2019-06-02 01:16:57 +0200

    bitstream++

--- convert/bitstream.hpp
+++ convert/bitstream.hpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <functional>
 #include <iostream>
 #include <vector>
 
@@ -9,7 +10,17 @@ namespace bitstream {
 
 class BitStreamWriter {
   public:
-    explicit BitStreamWriter(std::ostream& output) : cache_{0}, current_{0}, output_{output} {}
+    using Writer = std::function<void(std::byte const*, std::size_t)>;
+    explicit BitStreamWriter(Writer writer) : cache_{0}, current_{0}, writer_{move(writer)} {}
+    explicit BitStreamWriter(std::ostream& output) :
+		BitStreamWriter{
+			[out = std::ref(output)](std::byte const* data, size_t count) mutable
+			{
+              out.get().write((char const*) data, count);
+			}
+		}
+    {
+    }
 
     void write(std::vector<bool> const& bits)
     {
@@ -20,7 +31,7 @@ class BitStreamWriter {
     void write(bool bit)
     {
         if (bit)
-            cache_ |= (1 << ((sizeof(cache_) * 8) - current_));
+            cache_ |= (1llu << ((sizeof(cache_) * 8) - current_));
 
         current_++;
 
@@ -30,7 +41,7 @@ class BitStreamWriter {
 
     void flush()
     {
-        output_.write((char const*) &cache_, sizeof(cache_));
+        writer_((std::byte const *) &cache_, sizeof(cache_));
         current_ = 0;
         cache_ = 0;
     }
@@ -38,12 +49,21 @@ class BitStreamWriter {
   private:
     uint64_t cache_;
     size_t current_;
-    std::ostream& output_;
+    Writer writer_;
 };
 
 class BitStreamReader {
   public:
-    explicit BitStreamReader(std::istream& input) : cache_{}, input_{input} {}
+    using Reader = std::function<void(std::byte*, std::size_t)>;
+
+    explicit BitStreamReader(Reader reader) : cache_{}, current_{0}, reader_{move(reader)} {}
+
+    explicit BitStreamReader(std::istream& input)
+        : BitStreamReader{[in = std::ref(input)](std::byte* data, std::size_t count) mutable {
+              in.get().read((char*) data, count);
+          }}
+    {
+    }
 
     void read(std::vector<bool> bits, size_t count)
     {
@@ -54,17 +74,17 @@ class BitStreamReader {
     bool read()
     {
         if (current_ != sizeof(cache_) * 8)
-            return (cache_ & (1 << current_)) != 0;
+            return (cache_ & (1llu << current_)) != 0;
 
         current_ = 0;
-        input_.read((char*) &cache_, sizeof(cache_));
+        reader_((std::byte*) &cache_, sizeof(cache_));
     }
 
   private:
     // Not using vector<bool> here, as I cannot bulk-read into a vector<bool> conviniently.
     uint64_t cache_;
     size_t current_;
-    std::istream& input_;
+    Reader reader_;
 };
 
 }  // namespace bitstream
